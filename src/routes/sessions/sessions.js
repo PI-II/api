10648d67589bdae27b0a6d0b63e8cb2fa3c1a6fd
davMac606 <worksender053@gmail.com>
2024-11-25 15:05:48 -0300

    added a route to get sessions in that week. still working on it.

--- src/routes/sessions/sessions.js
+++ src/routes/sessions/sessions.js
@@ -62,18 +62,15 @@ export default async function (fastify) {
     );
   });
 
-  // const date1 = new Date(`September 21, 2024 16:30:00`);
-  // // Sun Dec 17 1995 03:24:00 GMT...
+  fastify.post("/week", (req, reply) => {
+    fastify.mysql.query(
+      `SELECT inicio, fim, usuario FROM ${db.database}.sessoes 
+      WHERE inicio LIKE ? AND fim LIKE ?`,
+      [`${req.body.inicio}%`, `${req.body.fim}%`],
+      function onResult(err, result) {
+      reply.send(err || result);
+      }
+    );
+  })
 
-  // const date2 = new Date(`2024-09-21T15:00:00`);
-  // // Sun Dec 17 1995 03:24:00 GMT...
-
-  // const time = date1 - date2; //30 minutos
-  // var minute = time / (1000 * 60);
-  // var hour = time / (1000 * 60 * 60);
-  // console.log("Início do treino: " + date1.toISOString());
-  // console.log("Término do treino: " + date2.toISOString());
-  // console.log("Duração do treino: " + minute.toString() + " minutos");
-
-  // console.log("Duração do treino: " + hour.toString() + " horas");
 }
